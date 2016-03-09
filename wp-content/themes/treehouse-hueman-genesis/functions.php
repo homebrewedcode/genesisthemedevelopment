100192b681f646c50137037b5461c9bf623fe317
Dale Pollitt <dale.negativezero@gmail.com>
2016-03-08 19:43:29 -0800

    Initial Commit

---
+++ wp-content/themes/treehouse-hueman-genesis/functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dale
+ * Date: 3/7/2016
+ * Time: 7:42 PM
+ */
+//* Start the engine
+include_once (get_template_directory() . '/lib/init.php');
+
+//* Chile theme (do not remove)
+define('CHILD_THEME_NAME', 'Treehouse Hueman Genesis');
+define('CHILD_THEME_URL', 'http://www.teamtreehouse.com');
+define('CHILD_THEME_VERSION', '1.0.0');
+
+//* Add HTML5 markup structure
+add_theme_support('html5', array('search-form', 'comment-form', 'comment-list'));
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support('genesis-responsive-viewport');
+
+//* Add support for 3-column footer widgets
+add_theme_support('genesis-footer-widgets', 3);
